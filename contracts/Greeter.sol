8e2618ba4861a33b6afb4f05a542da0f6b483876
sparsell <hello@sparsell.com>
2022-01-26 12:51:58 -0500

    added setGreeting function

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,7 +2,13 @@ pragma solidity >= 0.4.0 < 0.8.12;
 
 
 contract Greeter {
-    function greet() external pure returns(string memory) {
-        return "Hello World!";
+    string private _greeting = "Hello World!";
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
     }
 }
