1a1ba83f995aa22e7a36ce715a548885beff68c9
sparsell <hello@sparsell.com>
2022-02-02 14:24:37 -0500

    debugged syntax error

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,14 +1,17 @@
-pragma solidity >= 0.4.0 < 0.8.12; 
+ pragma solidity >= 0.4.0 < 0.8.12;
 
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract Greeter {
-    string private _greeting = "Hello World!";
+contract Greeter is Ownable {
+  string private _greeting = "Hello, World!";
 
-    function greet() external view returns(string memory) {
-        return _greeting;
-    }
+  function greet() external view returns(string memory) {
+    return _greeting;
+  }
 
-    function setGreeting(string calldata greeting) external {
-        _greeting = greeting;
-    }
+  function setGreeting(string calldata greeting) external onlyOwner {
+    _greeting = greeting;
+  }
 }
+
+
